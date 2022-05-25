4a388af8e250b4f9588b9caee08c464186fc8ae0
AlexisCuvillier <alexis.cuvillier@protonmail.com>
2022-05-25 13:51:26 +0200

    sign in

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Link } from "react-router-dom";
+import { UserContext } from '../context/userContext';
+
+export default function NavBar() {
+    const { toggleModals } = useContext(UserContext);
+    return (
+        <nav className="navbar navbar-light bg-light px-4">
+            <Link to="/" className='navbar-brand'>
+                AuthJs
+            </Link>
+            <div>
+                <button
+                    onClick={() => toggleModals("signUp")}
+                    className="btn btn-primary"
+                >
+                    Sign Up
+                </button>
+                <button
+                    onClick={() => toggleModals("signIn")}
+                    className="btn btn-primary ms-2"
+                >
+                    Sign In
+                </button>
+                <button
+
+                    className="btn btn-danger ms-2"
+                >
+                    Log Out
+                </button>
+            </div>
+        </nav>
+    )
+}
